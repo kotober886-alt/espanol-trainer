2af3adf02afc48a2a7022c201926e0a7f4b86ecc
kotober886-alt <kotober886@gmail.com>
2026-09-18 12:35:24 +0300

    Pin generated art loader to final raster cache key

--- scripts/inject_generated_art.py
+++ scripts/inject_generated_art.py
@@ -3,7 +3,7 @@ import re
 
 p = Path('index.html')
 s = p.read_text(encoding='utf-8')
-version = '20260918-runtime1'
+version = '20260918-raster-final2'
 
 css = f'<link rel="stylesheet" href="assets/generated-art.css?v={version}">'
 s = re.sub(
