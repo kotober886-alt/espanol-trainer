da61d6d92c7f541f0d3a5acb506564308b3f805c
kotober886-alt <kotober886@gmail.com>
2026-09-19 17:42:40 +0300

    Use standard card layout for beanie

--- assets/sprite-art.js
+++ assets/sprite-art.js
@@ -22,6 +22,7 @@
     foods: withVersion("assets/picture-labels/study-foods-v3.webp"),
     activities: withVersion("assets/picture-labels/study-activities-v3.webp"),
     clothesNew: withVersion("assets/picture-labels/study-clothes-new-v1.webp"),
+    beanieSingle: withVersion("assets/picture-labels/beanie.webp"),
     foodsExtra: withVersion("assets/picture-labels/study-foods-extra-v1.webp"),
     activitiesExtra: withVersion("assets/picture-labels/study-activities-extra-v1.webp")
   };
@@ -29,6 +30,7 @@
   const SPRITE_MAP={
     clothes:{tshirt:[0,0],shirt:[1,0],blouse:[2,0],sweater:[3,0],hood:[4,0],jacket:[5,0],coat:[0,1],dress:[1,1],skirt:[2,1],trousers:[3,1],jeans:[4,1],shorts:[5,1],suit:[0,2],pajamas:[1,2],underwear:[2,2],socks:[3,2],shoes:[4,2],sneakers:[5,2],boots:[0,3],sandals:[1,3],scarf:[2,3],gloves:[3,3],sunglasses:[4,3],belt:[5,3],tie:[0,4],bowtie:[1,4],hat:[2,4],cap:[3,4],backpack:[5,4]},
     clothesNew:{hoodie:[0,0],vest:[1,0],polo:[0,1],cardigan:[1,1]},
+    beanieSingle:{beanie:[0,0]},
     foods:{bread:[0,0],cheese:[1,0],egg:[2,0],milk:[3,0],rice:[4,0],pasta:[5,0],chicken:[6,0],fish:[0,1],meat:[1,1],potato:[2,1],tomato:[3,1],apple:[4,1],banana:[5,1],orange:[6,1],water:[0,2],coffee:[1,2],tea:[2,2],salt:[3,2],sugar:[4,2],bag:[5,2],bottle:[6,2],butter:[0,3],jar:[1,3],honey:[2,3],beans:[3,3],lentils:[4,3],soup:[5,3],salad:[6,3],sandwich:[0,4],pizza:[1,4],cookie:[2,4],cake:[3,4],icecream:[4,4],sausage:[5,4],shellfish:[6,4]},
     foodsExtra:{chocolate:[0,0],candy:[1,0],muffin:[2,0],flan:[0,1],juice:[1,1],soda:[2,1],wine:[0,2],beer:[1,2],puree:[2,2]},
     activities:{do_sport:[0,0],gym:[1,0],yoga:[2,0],run:[3,0],swim:[4,0],cycle:[5,0],football:[6,0],tennis:[0,1],dance:[1,1],videogames:[2,1],level:[2,1],boardgames:[3,1],cards:[4,1],chess:[5,1],puzzles:[6,1],books:[0,2],news:[1,2],notes:[2,2],languages:[3,2],learn:[3,2],spanish:[4,2],homework:[5,2],podcast:[6,2],draw:[0,3],paint:[1,3],photos:[2,3],videos:[3,3],stories:[4,3],crafts:[5,3],guitar:[6,3],cook:[0,4],movies:[1,4],series:[2,4],music:[3,4],walk:[4,4],travel:[5,4],rest:[0,5],friends:[1,5],hike:[2,5],camp:[3,5],plants:[4,5],dog:[5,5],picnic:[6,5]},
@@ -40,7 +42,6 @@
     size:"size.webp",
     fittingroom:"fittingroom.webp",
     bra:"bra.webp",
-    beanie:"beanie.webp",
     zipper:"zipper.webp",
     button:"button.webp",
     try_on:"try_on.webp",
@@ -145,6 +146,7 @@
     if(extra) return extra;
 
     return firstSprite([
+      ["beanieSingle","beanieSingle",id,1,1],
       ["clothes","clothes",id,6,5],
       ["clothesNew","clothesNew",id,2,2]
     ]);
